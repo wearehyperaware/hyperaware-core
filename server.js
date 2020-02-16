25124f321cf23c79c6a7d25dec63c59b881981a9
Jason James <jasongjames2012@hotmail.com>
2020-02-16 15:51:13 +0000

    added express server and websocket

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const io = require('socket.io')();
+const express = require('express');
+const bodyParser = require('body-parser');
+const server = express();
 
+server.use(bodyParser.urlencoded({ extended: false }));
+
+const http = server.listen(3001, () => {
+        console.log('Express server and socket.io websocket are running on localhost:3001');
+    }
+);
+
+const io = require('socket.io')(http);
+
+// Example websocket connection. Call subscribeToTimer from the browser (example in src/websocket-api)
 io.on('connection', (client) => {
     client.on('subscribeToTimer', (interval) => {
         console.log('client is subscribing to timer with interval ', interval);
@@ -9,6 +21,7 @@ io.on('connection', (client) => {
     });
 });
 
-const port = 8000;
-io.listen(port);
-console.log('listening on port ', port);
+// Example get request to express server
+server.get('/api/ping', async (req, res) => {
+    res.send("pong")
+});
--- src/websocket_api/timerExample.js
+++ src/websocket_api/timerExample.js
@@ -1,5 +1,5 @@
 import openSocket from 'socket.io-client';
-const  socket = openSocket('http://localhost:8000');
+const  socket = openSocket('http://localhost:3001');
 function subscribeToTimer(cb) {
     socket.on('timer', timestamp => cb(null, timestamp));
     socket.emit('subscribeToTimer', 1000);
