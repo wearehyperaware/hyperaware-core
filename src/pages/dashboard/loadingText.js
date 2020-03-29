b2de860db7f8e77afe7f2af407c6bc311d227a2d
Jason James <jasongjames2012@hotmail.com>
2020-03-29 18:01:02 +0100

    tweak loading messages

--- src/pages/dashboard/loadingText.js
+++ src/pages/dashboard/loadingText.js
@@ -2,22 +2,24 @@ export function setLoadingText() {
 
     document.getElementById("loading-text").style.display = "block"
 
-    document.getElementById("dashboardLoadingText").innerHTML = "Loading Vehicles from IoTeX DID registry...";
+    document.getElementById("dashboardLoadingText").innerHTML = "Loading Vehicles from IoTeX...";
     setTimeout(function () {
         document.getElementById("dashboardLoadingText").innerHTML = "Retrieving DID Documents from the Arweave permaweb...";
-    }, 2000);
+    }, 2400);
     setTimeout(function () {
         document.getElementById("dashboardLoadingText").innerHTML = "Loading Zone DID documents from Ethereum...";
     }, 5000);
     setTimeout(function () {
+        document.getElementById("dashboardLoadingText").innerHTML = "Retrieving GeoJSON files from Zone DID documents...";
+    }, 7800);
+    setTimeout(function () {
         document.getElementById("dashboardLoadingText").innerHTML = "Assigning random routes to demo vehicles...";
-    }, 8000);
+    }, 11500);
     setTimeout(function () {
         document.getElementById("dashboardLoadingText").innerHTML = "Populating map with vehicles and zones...";
-    }, 10000);
+    }, 14000);
     setTimeout(function () {
         document.getElementById("dashboardLoadingText").innerHTML = "(Yes, we know it's slow at the moment)";
-    }, 13000);
-    // setTimeout(function () {
-    // }, 15000);
+    }, 16000);
+
 }
