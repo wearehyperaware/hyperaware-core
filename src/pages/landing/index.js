cfd5db1721a04eea1f61d6372580ca163e8fbd7c
Jason James <jasongjames2012@hotmail.com>
2020-03-30 17:29:49 +0100

    contact us updated

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -293,68 +293,31 @@ class Index extends Component {
                             </Col>
 
                             <Col lg={5} md={6} className="mt-4 mt-sm-0 pt-2 pt-sm-0 order-2 order-md-1">
-                                <div className="pt-5 pb-5 p-4 bg-light shadow rounded">
-                                    <h4>Get In Touch !</h4>
-                                    <div className="custom-form mt-4">
-                                        <div id="message"></div>
-                                        <Alert color="info" isOpen={this.state.Contactvisible} toggle={() => {
-                                            this.setState({Contactvisible: !this.state.Contactvisible})
-                                        }}>
-                                            Contact details send successfully.
-                                        </Alert>
-                                        <form method="post" onSubmit={this.handleSubmit} name="contact-form"
-                                              id="contact-form">
-                                            <Row>
-                                                <Col md={6}>
-                                                    <div className="form-group position-relative">
-                                                        <label>Your Name <span className="text-danger">*</span></label>
-                                                        <i className="mdi mdi-account ml-3 icons"></i>
-                                                        <input name="name" id="name" type="text"
-                                                               className="form-control pl-5" placeholder="First Name :"
-                                                               required/>
-                                                    </div>
-                                                </Col>
-                                                <Col md={6}>
-                                                    <div className="form-group position-relative">
-                                                        <label>Your Email <span className="text-danger">*</span></label>
-                                                        <i className="mdi mdi-email ml-3 icons"></i>
-                                                        <input name="email" id="email" type="email"
-                                                               className="form-control pl-5" placeholder="Your email :"
-                                                               required/>
-                                                    </div>
-                                                </Col>
-                                                <Col md={12}>
-                                                    <div className="form-group position-relative">
-                                                        <label>Subject</label>
-                                                        <i className="mdi mdi-book ml-3 icons"></i>
-                                                        <input name="subject" id="subject" className="form-control pl-5"
-                                                               placeholder="Your subject :" required/>
-                                                    </div>
-                                                </Col>
-                                                <Col md={12}>
-                                                    <div className="form-group position-relative">
-                                                        <label>Comments</label>
-                                                        <i className="mdi mdi-comment-text-outline ml-3 icons"></i>
-                                                        <textarea name="comments" id="comments" rows="4"
-                                                                  className="form-control pl-5"
-                                                                  placeholder="Your Message :"></textarea>
-                                                    </div>
-                                                </Col>
-                                            </Row>
-                                            <Row>
-                                                <Col sm={12} className="text-center">
-                                                    <input type="submit" id="submit" name="send"
-                                                           className="submitBnt btn btn-primary btn-block"
-                                                           value="Send Message"/>
-                                                    <div id="simple-msg"></div>
-                                                </Col>
-                                            </Row>
-                                        </form>
+                                    <div className="title-heading ml-lg-4">
+                                        <h4 className="mb-4">Contact Details</h4>
+                                        <p className="text-muted">If you'd like to know more, get in touch.</p>
+                                        <div className="contact-detail mt-3">
+                                            <div className="icon mt-3 float-left">
+                                                <i className="mdi mdi-email-variant text-muted mdi-36px mr-3"></i>
+                                            </div>
+                                            <div className="content mt-3 overflow-hidden d-block">
+                                                <h4 className="title font-weight-bold mb-0">Email</h4>
+                                                <a href="mailto:contact@example.com" className="text-primary h6">trust@hyperaware.io</a>
+                                            </div>
+                                        </div>
+
+                                        <div className="contact-detail mt-3">
+                                            <div className="icon mt-3 float-left">
+                                                <i className="mdi mdi-map-marker-outline text-muted mdi-36px mr-3"></i>
+                                            </div>
+                                            <div className="content mt-3 overflow-hidden d-block">
+                                                <h4 className="title font-weight-bold mb-0">Location</h4>
+                                                <a href="mailto:contact@example.com" className="text-primary h6">London, England</a>
+
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </Col>
-
-
+                                </Col>
                         </Row>
                     </div>
                 </section>
